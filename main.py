e5f73c59bd754235351284276a0e1c04378b16b7
Alek Michelson <am7590@rit.edu>
2022-06-12 15:25:18 -0400

    Buy and sell times

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 from binance import Client
 import matplotlib.pyplot as plt
+
 client = Client()
+
 
 # Get hourly prices over last 400 hours
 def get_data(symbol, interval='1h', lookback='400'):
-    frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback+' hours UTC'))
-    frame = frame.iloc[:,0:6]
+    frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback + ' hours UTC'))
+    frame = frame.iloc[:, 0:6]
     frame.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
     frame.set_index('Time', inplace=True)
     frame.index = pd.to_datetime(frame.index, unit='ms')
@@ -15,5 +17,35 @@ def get_data(symbol, interval='1h', lookback='400'):
     return frame
 
 
+def populate_dataframe(df):
+    df['rollhigh'] = df.High.rolling(15).max()  # rolling max over last 15 hours
+    df['rolllow'] = df.Low.rolling(15).min()  # rolling low over last 15 hours
+    df['mid'] = (df.rollhigh + df.rolllow) / 2
+    df['high_approach'] = np.where(df.Close > df.rollhigh * 0.996, 1, 0)  # -0.4%
+    df['close_above_mid'] = np.where(df.Close > df.mid, 1, 0)
+    df['mid_cross'] = df.close_above_mid.diff() == 1
+
+def entries_exits(df):
+    in_position = False
+    buy_dates, sell_dates = [], []
+
+    for i in range(len(df)):
+        if not in_position:  # Buy condition
+            if df.iloc[i].mid_cross:
+                buy_dates.append(df.iloc[i + 1].name)
+                in_position = True
+        if in_position:
+            if df.iloc[i].high_approach:
+                sell_dates.append(df.iloc[i + 1].name)
+                in_position = False
+
+    print(buy_dates)
+    print(sell_dates)
+
+
 if __name__ == '__main__':
-    print(get_data("BTCUSDT"))
+    dataframe = get_data("BTCUSDT")
+    populate_dataframe(dataframe)
+    entries_exits(dataframe)
+
+
